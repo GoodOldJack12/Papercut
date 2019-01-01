bef2f48af148bf271824d2cf4d6161792781debf
GoodOldJack12 <deloght.jonathan@gmail.com>
2019-01-01 16:36:23 +0100

    Easy constructor and default initialization

--- Domain/Containers/Bookshelf.cs
+++ Domain/Containers/Bookshelf.cs
@@ -10,9 +10,9 @@ namespace Domain.Containers
         public int Id { get; set; }
         public string Name { get; set; }
         public int Capacity { get; set; }
-        public List<ITrackedItem> Items { get; set; }
+        public List<ITrackedItem> Items { get; set; } = new List<ITrackedItem>();
         public IEnumerable<Type> AcceptedItems { get; set; }= new[] {typeof(Book)};
-        public IEnumerable<IStorage> SubStorages { get; set; }
+        public IEnumerable<IStorage> SubStorages { get; set; } = new List<IStorage>();
         public IStorage GetStorageFor(ITrackedItem item)
         {
             if (AcceptedItems.Contains(item.GetType())&& Items.Count() < Capacity)
@@ -20,6 +20,12 @@ namespace Domain.Containers
                 return this;
             }
             return null;
-        }        
+        }
+
+        public Bookshelf(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;   
+        }
     }
 }
